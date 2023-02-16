38f10b03eb3fca32e999a006f234498925bb61af
Stefan Schubert <stefan.schubert@prodyna.com>
2023-02-16 17:07:00 +0100

    Trying out react-oauth/google

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import {BookPreview} from "./books/book-preview";
+import {GoogleLogin} from "@react-oauth/google";
 
 function App() {
-  return (
-      <div className="App">
-        <BookPreview name="Name" description="Description"/>
-      </div>
-  );
+    return (
+        <div className="App">
+            <GoogleLogin onSuccess={resp => console.log(resp)} onError={() => console.error("Nope")}/>
+            <BookPreview name="Name" description="Description"/>
+        </div>
+    );
 }
 
 export default App;
